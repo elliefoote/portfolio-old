e924d1a5ff48bedaf7a319d0a0701233acd3c364
Ellie Foote <eleanorjfoote@gmail.com>
2022-03-28 17:57:53 +0200

    projects section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <div id="hero" className="container d-flex flex-column justify-content-center">
         <h1 className="display-1 hero-header">Hi, I'm Ellie.</h1>.
         <div id="hero-body" className="display-5">I'm a software engineer who loves building beautiful, responsive full-stack apps.</div>
-        <div className="mt-5"><a id="cta" href="#my-work" className="fs-4 py-2 px-3 text-light">See my work</a></div>
+        <div className="mt-5"><a href="#my-work" className="cta fs-4 py-2 px-3 text-light">See my work</a></div>
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,83 @@
-import * as React from "react"
-
+import * as React from "react";
+import BookSwapper from '../images/BookSwapper.png';
+import EManager from '../images/EManager.png';
+import Homegrown from '../images/Homegrown.png';
+import MarcTinent from '../images/MarcTinent.png';
+import { StaticImage } from "gatsby-plugin-image";
 
 const Projects = () => {
-  return (
-    <div id="my-work">
+    const projectData = [
+        {
+            id: 1,
+            title: 'BookSwapper',
+            description: 'Full stack app that allows users to list their pre-loved books and exchange messages to arrange book swaps.',
+            liveLink: 'https://ejf-book-swapper.herokuapp.com/',
+            codeLink: 'https://github.com/elliefoote/book-swap',
+            img: BookSwapper,
+            leftOrRight: 'left'
+        },
+        {
+            id: 2,
+            title: 'E-Manager',
+            description: 'Full stack app that allows small business owners to manage clients and allocate tasks.',
+            liveLink: 'https://ejf-emanager.herokuapp.com/',
+            codeLink: 'https://github.com/elliefoote/e-manager-app',
+            img: EManager,
+            leftOrRight: 'right'
+        },
+        {
+            id: 3,
+            title: 'Homegrown Bcn',
+            description: 'Full stack ecommerce app that allows socially conscious shoppers to connect with local makers and artists.',
+            liveLink: 'https://homegrown-bcn.herokuapp.com/',
+            codeLink: 'https://github.com/elliefoote/e-manager-app',
+            img: Homegrown,
+            leftOrRight: 'left'
+        },
+        {
+            id: 4,
+            title: 'MarcTinent.com',
+            description: 'One-page site for a professional author.',
+            liveLink: 'https://www.marctinent.com',
+            codeLink: 'https://github.com/elliefoote/marc-tinent/',
+            img: MarcTinent,
+            leftOrRight: 'right'
+        }
+    ]
+  
+    return (
+    <div id="my-work" className="container">
 
-        <h2 className="display-3 mb-4 text-dark" >Projects</h2>
+        <h2 className="display-3 mb-4 text-dark">Projects</h2>
 
+        <div className="container mt-5">
+
+        {
+
+            projectData.map(p => (
+
+                <div className={p.leftOrRight==="left" ? "project-card row justify-content-center gx-5 mb-5" : "project-card row justify-content-center gx-5 mb-5 flex-row-reverse"} key={p.id}>
+
+                    <div className="project-image-container col-8">
+                    <img alt={p.title} src={p.img} className="project-image" />
+                    </div>
+
+                    <div className="col-4 border">
+                        <div className="fs-3">{p.title}</div>
+                        <div className="fs-6">{p.description}</div>
+                        <div className="mt-4"><a href={p.liveLink} className="cta text-light py-2 px-3 fs-6 me-3">View Live Version</a> <a href={p.codeLink} className="gh-link">View On GitHub</a></div>
+                    </div>
+
+
+                </div>
+
+            ))
+
+
+
+        }
+
+        </div>
         
     </div>
   )
