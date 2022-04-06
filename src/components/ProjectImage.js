eaad8ae38bbbd217cb573c7c8f121e70ea1f746c
Ellie Foote <eleanorjfoote@gmail.com>
2022-04-06 13:50:44 +0200

    removed unnecessary js

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -25,13 +25,9 @@ export default function ProjectImage({filename, alt}) {
           }
         `}
         render = {data => {
-            console.log('The filename is ' + filename);
             const file = data.images.edges.find((edge) => {
-                console.log('The relative path is ' + edge.node.relativePath);
                 return edge.node.relativePath.includes(filename)});
-            console.log('The file is ' + file.node.name);
-            const image = getImage(file.node);
-            console.log(image);        
+            const image = getImage(file.node);       
             if (!image) {return null};
             return <GatsbyImage image={image} alt={alt} />;
             }}
