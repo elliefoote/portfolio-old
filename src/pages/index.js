6017b430856b45e9113e664b4a9088ee6ec714bc
Ellie Foote <eleanorjfoote@gmail.com>
2022-04-19 15:26:18 +0200

    try again with meta image

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => {
           <meta charSet="utf-8" />
           <html lang="en" />
           <meta name="description" content="I'm a software engineer based in Barcelona, building responsive, user-friendly web sites and apps." />
-          <meta name="image" property="og:image" content="/metaImage.png" />
+          <meta name="image" property="og:image" content={"/social-share.png"} />
           <title>Ellie Foote | Software Engineer</title>
           <link rel="canonical" href="https://www.elliefoote.com" />
         </Helmet>
