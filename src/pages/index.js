ead05b7151c2bb3c214f0c561788a3b9fb7fa628
Ellie Foote <eleanorjfoote@gmail.com>
2022-04-19 15:40:07 +0200

    meta image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Navbar from '../components/Navbar';
 import App from '../components/App';
 import './styles.css';
 import Footer from '../components/Footer';
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+import SocialShare from '../images/social-share.png';
 
 // markup
 const IndexPage = () => {
@@ -13,7 +14,7 @@ const IndexPage = () => {
           <meta charSet="utf-8" />
           <html lang="en" />
           <meta name="description" content="I'm a software engineer based in Barcelona, building responsive, user-friendly web sites and apps." />
-          <meta name="image" property="og:image" content={"/social-share.png"} />
+          <meta name="image" property="og:image" content={SocialShare} />
           <title>Ellie Foote | Software Engineer</title>
           <link rel="canonical" href="https://www.elliefoote.com" />
         </Helmet>
