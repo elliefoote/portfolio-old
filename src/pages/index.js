5c6dc93ae30477032170cc6e1538911f0429974e
Ellie Foote <eleanorjfoote@gmail.com>
2022-09-30 19:37:02 +0200

    rm footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,29 @@
-import * as React from "react"
-import Navbar from '../components/Navbar';
-import App from '../components/App';
-import './styles.css';
-import Footer from '../components/Footer';
-import Helmet from 'react-helmet';
-import SocialShare from '../images/social-share.png';
+import * as React from "react";
+import Navbar from "../components/Navbar";
+import App from "../components/App";
+import "./styles.css";
+import Helmet from "react-helmet";
+import SocialShare from "../images/social-share.png";
 
 // markup
 const IndexPage = () => {
-  return (  
+  return (
     <main>
       <Helmet>
-          <meta charSet="utf-8" />
-          <html lang="en" />
-          <meta name="description" content="I'm a software engineer based in Barcelona, building responsive, user-friendly web sites and apps." />
-          <meta name="image" property="og:image" content={SocialShare} />
-          <title>Ellie Foote | Software Engineer</title>
-          <link rel="canonical" href="https://www.elliefoote.com" />
-        </Helmet>
+        <meta charSet="utf-8" />
+        <html lang="en" />
+        <meta
+          name="description"
+          content="I'm a software engineer based in Barcelona, building responsive, user-friendly web sites and apps."
+        />
+        <meta name="image" property="og:image" content={SocialShare} />
+        <title>Ellie Foote | Software Engineer</title>
+        <link rel="canonical" href="https://www.elliefoote.com" />
+      </Helmet>
       <Navbar />
       <App />
-      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
